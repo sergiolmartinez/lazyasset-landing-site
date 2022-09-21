544dc99db87a8837a0b635e7201ab55ffa2aa9e7
sergiolmartinez <smart0586@gmail.com>
2022-09-20 21:41:35 -0700

    centered twitter link, made it responsive

--- landing-page-v1/src/components/Footer.js
+++ landing-page-v1/src/components/Footer.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import './Footer.css'
-import { FaTwitter } from 'react-icons/fa'
+import { FaTwitter, FaDiscord } from 'react-icons/fa'
 
 export const Footer = () => {
   return (
     <footer className='footer'>
-        <span className='footer_content'>
-        <FaTwitter className='twitter_logo' size={23} color='#1DA1F2' />
-        <a href='https://twitter.com/LazyAsset' className='twitter_link' target='_blank' rel="noreferrer noopener">@lazyasset</a>
+        <span className='footer_content'> 
+        {/* <a href='#' className='links' target='_blank' rel="noreferrer noopener">
+          <FaDiscord className='discord_logo' size={35} color='#5865F2'/>
+        </a> */}
+        
+        <a href='https://twitter.com/LazyAsset' className='links' target='_blank' rel="noreferrer noopener">
+          <FaTwitter className='twitter_logo' size={35} color='#1DA1F2' />
+        </a>
         </span>
     </footer>
   )
--- landing-page-v1/src/components/Hero.js
+++ landing-page-v1/src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Hero.css';
+import icon_blue from '../assets/icon_blue.png';
 
 export const Hero = ({imageSrc}) => {
     return (
@@ -8,6 +9,10 @@ export const Hero = ({imageSrc}) => {
                 <img src={imageSrc} alt='Blue Ripple' className='hero_image' />
             </div>
             <div className='column_middle' >
+            {/* <span className='logo'>
+                <img src={icon_blue} alt='blue logo' className='logo_image' />
+                LAZY ASSET
+            </span> */}
             {/* <h1 className='hero_title'>NFTs made simple.</h1> */}
                 <h1 className='hero_title' id='textCycle'></h1>
             </div>
